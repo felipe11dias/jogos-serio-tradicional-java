3abbaca1b803a9ba63c7ebaeddeb10c7e13bc99c
Felipe Dias <clame1997mac@hotmail.com>
2023-11-10 00:53:11 -0300

    Update and rename WebConfig.java to CorsConfig.java

--- src/main/java/br/unifor/enviromentgameserius/tcc/config/CorsConfig.java
+++ src/main/java/br/unifor/enviromentgameserius/tcc/config/CorsConfig.java
@@ -6,11 +6,15 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
     }
 }
