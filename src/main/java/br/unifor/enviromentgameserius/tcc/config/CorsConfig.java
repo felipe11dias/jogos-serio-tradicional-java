3ee36f3a2056c1d1ae04d5e4346e4c2bc0352479
Felipe Dias <felipemacdowell@unifor.br>
2023-11-10 01:18:48 -0300

    Cors Config

--- src/main/java/br/unifor/enviromentgameserius/tcc/config/CorsConfig.java
+++ src/main/java/br/unifor/enviromentgameserius/tcc/config/CorsConfig.java
@@ -2,7 +2,6 @@ package br.unifor.enviromentgameserius.tcc.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedMethods("*")
             .allowedHeaders("*")
             .allowCredentials(true)
             .maxAge(3600);
